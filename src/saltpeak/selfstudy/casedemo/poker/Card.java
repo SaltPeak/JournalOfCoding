d77d4e496ccceba3067b5e0a33b258b1095c2ed3
Big Hit <46077692+WizardValley@users.noreply.github.com>
2020-04-15 07:25:21 +0800

    create

--- src/saltpeak/selfstudy/casedemo/poker/Card.java
+++ src/saltpeak/selfstudy/casedemo/poker/Card.java
@@ -10,12 +10,12 @@ package saltpeak.selfstudy.casedemo.poker;
 public class Card {
     private byte suit, rank;
     public static final byte MODE = 4, MIN_CODE = 0,MAX_CODE = 53, ERROR_CODE = -1;//
-    public static final char[] SUITS = {'♠','♥','♣','♦'};//扑克牌花色列表
+    public static final char[] SUITS = {'♠','♥','♣','♦'};//扑克牌花色列表：桃(Spade)、心(Heart)、梅(Club)、方(Diamond)
     public static final String[] RANKS = {"A","2","3","4","5","6","7","8","9","10","J","Q","K","G"};//扑克牌面值序列
 
     public Card(byte code) {
         if((code>=MIN_CODE)&&(code<=MAX_CODE)){
-            suit =(byte)(code%MODE);//byte与byte求余是int?
+            suit =(byte)(code%MODE);//byte与byte求余结果是int?
             rank = (byte)(code/MODE);
         }
         else{
