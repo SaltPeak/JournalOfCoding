b1e1d6256d02cee71f7d8710a60e73ad03ec4525
Big Hit <46077692+WizardValley@users.noreply.github.com>
2020-04-15 12:28:42 +0800

    create

--- src/saltpeak/selfstudy/basicsyntax/HelloWorld.java
+++ src/saltpeak/selfstudy/basicsyntax/HelloWorld.java
@@ -8,16 +8,16 @@ package saltpeak.selfstudy.basicsyntax;
  * @created 2020/4/13 21:30
  */
 /*
-Knowledge repository:
+Knowledge Repository:
     1. Application Software Usually Contains Program & Document.
         1.1. Program Is  Instruction Sequence, and always Composes of  Business Logic(Domain-Specific Knowledge or Algorithm) & Data Structure.
         1.2. Document Refers to Usage Manual & Installation & Configuration Illustration.
     2. Code Style: Indents & Spaces & Alignment -> Imports & Blocks &
         2.1. Tabs & Indents :
         2.2. Block Style: next-line style & end-of-line style.
-      3. Citation Category: Wildcard Citation & Specific Citation.
-      4. Error Type: Syntax Error & Runtime Error & Logical Error.
-      5. Token Class: Keyword / Identifier / Punctuation / Delimiter / Comment / Annotation
+    3. Citation Category: Wildcard Citation & Specific Citation.
+    4. Error Type: Syntax Error & Runtime Error & Logical Error.
+    5. Token Class: Keyword / Identifier / Punctuation / Delimiter / Comment / Annotation
 */
 public class HelloWorld {
     //The main method is the entrance of executing a program.
