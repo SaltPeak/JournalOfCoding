bc982e1f2948f3aba5da1773c540f9fc08aa2416
Big Hit <46077692+WizardValley@users.noreply.github.com>
2020-04-19 23:02:46 +0800

    create

---
+++ src/saltpeak/selfstudy/basicsyntax/SimpleCalculation.java
@@ -0,0 +1,14 @@
+package saltpeak.selfstudy.basicsyntax;
+
+/**
+ * @author BigHit from SaltPeak
+ * @category The "saltpeak.selfstudy.basicsyntax" Package of "Journal Of Coding" Project
+ * @github SaltPeak
+ * @domain
+ * @created 2020/4/19 22:53
+ */
+public class SimpleCalculation {
+    public static void main(String[] args){
+        System.out.println((10.5+2*3)/(45-3.5));
+    }
+}
